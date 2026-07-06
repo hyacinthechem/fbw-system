4b769ff1428156b746e9ce04c73b12c64a2a3ff2
Hyacinthe Chemasle <hyacinthechem@gmail.com>
2026-07-07 00:46:42 +0200

    Draft: add FBW system setup

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "left_aileron.h"
+#include "right_aileron.h"
+#include "stabiliser.h"
+#include "vertical.h"
+#include "horizontal.h"
+
+using namespace std;
+
+int main() {
+
+
+ return 0;
+}
